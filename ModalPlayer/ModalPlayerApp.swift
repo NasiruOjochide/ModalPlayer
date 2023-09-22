b7032a0ab34ba627bc466f73a820add6b418a3e9
NasiruOjochide <nasiruojochide@gmail.com>
2023-09-22 07:06:44 +0100

    Initial Commit

---
+++ ModalPlayer/ModalPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModalPlayerApp.swift
+//  ModalPlayer
+//
+//  Created by Danjuma Nasiru on 22/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct ModalPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
