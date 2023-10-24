e7e2b7a6862fddc2b40353eda18581f0fa9ce40a
NasiruOjochide <nasiruojochide@gmail.com>
2023-10-24 11:07:47 +0100

    equaliser animation added

---
+++ ModalPlayer/Source/Player/Views/EqualizerAnimationView.swift
@@ -0,0 +1,35 @@
+//
+//  EqualizerAnimationView.swift
+//  ModalPlayer
+//
+//  Created by Danjuma Nasiru on 24/10/2023.
+//
+
+import SwiftUI
+
+struct EqualizerAnimationView: View {
+    @Environment(\.refresh) var refresh
+    @State private var columnHeight: CGFloat = 100
+    
+    let timer = Timer.publish(every: 0.5, on: .main, in: .common).autoconnect()
+    
+    var body: some View {
+        HStack(alignment: .bottom) {
+            ForEach(1..<5) { item in
+                VStack {}
+                    .frame(width: 30, height: (columnHeight / CGFloat (Int.random(in: 1...3))))
+                .background(.red)
+                .padding()
+            }
+        }
+        .onReceive(timer) { time in
+            columnHeight = CGFloat(Int.random(in: 30...100))
+        }
+    }
+}
+
+struct EqualizerAnimationView_Previews: PreviewProvider {
+    static var previews: some View {
+        EqualizerAnimationView()
+    }
+}
