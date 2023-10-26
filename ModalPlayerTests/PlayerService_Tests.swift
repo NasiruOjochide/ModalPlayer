461d9041f486ded003fde81b83e18eeef22146d2
NasiruOjochide <nasiruojochide@gmail.com>
2023-10-26 12:19:14 +0100

    add tests module to project

---
+++ ModalPlayerTests/PlayerService_Tests.swift
@@ -0,0 +1,29 @@
+//
+//  PlayerService_Tests.swift
+//  ModalPlayerTests
+//
+//  Created by Danjuma Nasiru on 26/10/2023.
+//
+
+import XCTest
+@testable import ModalPlayer
+
+final class PlayerService_Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        
+    }
+
+    func testPerformanceExample() throws {
+        
+    }
+
+}
